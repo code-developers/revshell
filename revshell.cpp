f7529374b3a7f8c07d5f21d487ba70a69549e721
krishpranav <krisna.pranav@gmail.com>
2021-04-18 16:40:28 +0530

    added includes and mentions some variables

---
+++ revshell.cpp
@@ -0,0 +1,16 @@
+// includes
+#include <stdio.h>
+#include <string.h>
+#include <tchar.h>
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <Windows.h>
+
+#pragma comment(lib, "ws2_32")
+
+WSADATA wsaData;
+SOCKET sock;
+STARTUPINFO startInfo;
+PROCESS_INFORMATION procInfo;
+struct sockaddr_in sockAddr;
